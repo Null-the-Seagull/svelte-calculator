99db6979ba2796dd87b367cc0e1d015e7c45d15f
Saiyanide <robertsonliam98@gmail.com>
2021-03-28 21:45:44 +1100

    Cleared readme and added title

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
 	export let name;
+	let title = 'Svelte Calculator'
 </script>
 
 <main>
+	<h1>{title}</h1>
+
+
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
